cc9873da0fee60f2edf58d119c211851369fdff0
59114_Chetana <deslechetna1999@gmail.com>
2022-05-09 20:59:39 +0530

    [Chetana] Refactor : Email is valid or not

--- EmailValidateDemo/Program.cs
+++ EmailValidateDemo/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(String[] args)
     {
         CheckEmail pattern = new CheckEmail();
-        //pattern.CheckEmail("Chetana+des@gmail.com.in");
-        //pattern.CheckEmail("Chetanadesle1999@gmail.com.in");
-        //pattern.CheckEmail("abc");
-        // pattern.CheckEmail("abc@bridgelabz");
-        // pattern.CheckEmail("abc@bridgelabz.co");
+        //pattern.ValidateEmail("Chetana+des@gmail.com.in");
+        //pattern.ValidateEmail("Chetanadesle1999@gmail.com.in");
+        //pattern.ValidateEmail("abc");
+        //pattern.ValidateEmail("abc@bridgelabz");
+        //pattern.ValidateEmail("abc@bridgelabz.co");
         bool result = pattern.ValidateEmail("abc.xyz@bridgelabz.co.in");
         if (result == true)
         {
