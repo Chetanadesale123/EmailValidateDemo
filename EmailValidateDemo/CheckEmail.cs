f06eb77d195ad38a52edd914b6f793ba5c60a0a4
59114_Chetana <deslechetna1999@gmail.com>
2022-05-09 20:50:51 +0530

    [Chetana] Added : Email is valid or not

---
+++ EmailValidateDemo/CheckEmail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailValidateDemo
+{
+    public class CheckEmail
+    {
+        //Chetana+des@gmail.com.in
+        public static string EMAIL_REGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
+        //public static string EMAIL_REGEX = "^[a-z]{3,}$";
+        //public static string EMAIL_REGEX = "^[a-z]{3,}[@][a-zA-Z]{6,}$";
+        //public static string EMAIL_REGEX = "^[a-z]{3,}[@][a-zA-Z]{6,}[.][a-zA-Z]{2,3}$";
+        //public static string EMAIL_REGEX = "^[a-z]{3,}+[.-+_]{0,1}[0-9a-zA-Z]+[@]+[a-zA-Z]{6,}[.][a-zA-Z]{2,3}+[.][a-zA-Z]{2,3}$";
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, EMAIL_REGEX);
+        }
+    }
+}
--- EmailValidateDemo/Program.cs
+++ EmailValidateDemo/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmailValidateDemo;
+using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+        CheckEmail pattern = new CheckEmail();
+        //pattern.CheckEmail("Chetana+des@gmail.com.in");
+        //pattern.CheckEmail("Chetanadesle1999@gmail.com.in");
+        //pattern.CheckEmail("abc");
+        // pattern.CheckEmail("abc@bridgelabz");
+        // pattern.CheckEmail("abc@bridgelabz.co");
+        bool result = pattern.ValidateEmail("abc.xyz@bridgelabz.co.in");
+        if (result == true)
+        {
+            Console.WriteLine("Email Matches");
+        }
+        else
+        {
+            Console.WriteLine("Verify Email Again");
+        }
+    }
+}
